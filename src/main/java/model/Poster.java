84f123e3edbcfabf369fc48abc93537cd66f3548
AbiPetheram <abi.gill97@gmail.com>
2024-09-26 15:46:40 +0100

    feat: add Poster class

---
+++ src/main/java/model/Poster.java
@@ -0,0 +1,15 @@
+package model;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class Poster {
+}
