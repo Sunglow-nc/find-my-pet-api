2f32dcdd09ef6b1cc79b151a9d8b44dc0fa464ae
qiuqiu <qiujuanwang87@gmail.com>
2024-09-26 14:23:44 +0100

    Initial Commit

---
+++ src/main/java/com/sunglow/find_my_pet/FindMyPetApplication.java
@@ -0,0 +1,13 @@
+package com.sunglow.find_my_pet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindMyPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindMyPetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sunglow/find_my_pet/FindMyPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunglow.find_my_pet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindMyPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
