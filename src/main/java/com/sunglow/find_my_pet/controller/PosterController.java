34fb4b5e36440668a3f812668427128a26b87b6f
andrei-vasiliu-coding <andrei-vasiliu@outlook.com>
2024-09-26 15:58:40 +0100

    feat: add posterService and GET for all posters

--- src/main/java/com/sunglow/find_my_pet/controller/PosterController.java
+++ src/main/java/com/sunglow/find_my_pet/controller/PosterController.java
@@ -1,12 +1,26 @@
 package com.sunglow.find_my_pet.controller;
 
+import com.sunglow.find_my_pet.model.Poster;
+import com.sunglow.find_my_pet.service.PosterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
 public class PosterController {
 
+    @Autowired
+    PosterService posterService;
+
+    @GetMapping("/posters")
+    public ResponseEntity<List<Poster>> getAllPosters() {
+        return new ResponseEntity<List<Poster>>(posterService.getAllAlbums(), HttpStatus.OK);
+    }
 
 }
