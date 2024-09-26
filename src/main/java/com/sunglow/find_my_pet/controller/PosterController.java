7b441312e902a1f46957f5591d2ac23d5d34cead
andrei-vasiliu-coding <andrei-vasiliu@outlook.com>
2024-09-26 15:36:22 +0100

    feat: create controller package and class with base url

---
+++ src/main/java/com/sunglow/find_my_pet/controller/PosterController.java
@@ -0,0 +1,12 @@
+package com.sunglow.find_my_pet.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class PosterController {
+
+
+}
