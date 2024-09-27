37b5b18f17a59b5f3f95eab9b9c1a2f20ad594d5
andrei-vasiliu-coding <andrei-vasiliu@outlook.com>
2024-09-27 16:26:55 +0100

    fix: ensure PetManagerRepository and OwnerManagerRepository are annotated with @Repository

--- src/main/java/com/sunglow/find_my_pet/repository/PetManagerRepository.java
+++ src/main/java/com/sunglow/find_my_pet/repository/PetManagerRepository.java
@@ -4,8 +4,9 @@ import com.sunglow.find_my_pet.model.Pet;
 import com.sunglow.find_my_pet.model.Poster;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.Repository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PetManagerRepository extends CrudRepository<Pet, Long> {
 
 }
