4aa5849e86c9fce3325db8b0f8e52392e6de8592
Oliver Arroyo <o.t.arroyo@gmail.com>
2024-09-28 11:40:48 +0100

    style: reformat the file

--- src/main/java/com/sunglow/find_my_pet/service/PosterService.java
+++ src/main/java/com/sunglow/find_my_pet/service/PosterService.java
@@ -1,7 +1,6 @@
 package com.sunglow.find_my_pet.service;
 
 import com.sunglow.find_my_pet.model.Poster;
-import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,7 +8,10 @@ import java.util.Optional;
 public interface PosterService {
 
     List<Poster> getAllPosters();
+
     Poster getPosterById(Long id);
+
     Poster insertPoster(Poster poster);
+
     Optional<Poster> updatePoster(Long id, Poster poster);
 }
