53db9d1f40ab21b7b12bb444dd549f3e3f05aa42
qiuqiu <qiujuanwang87@gmail.com>
2024-09-30 15:00:12 +0100

    feat: add findByPet in PosterManagerRepository interface

--- src/main/java/com/sunglow/find_my_pet/repository/PosterManagerRepository.java
+++ src/main/java/com/sunglow/find_my_pet/repository/PosterManagerRepository.java
@@ -1,9 +1,14 @@
 package com.sunglow.find_my_pet.repository;
 
+import com.sunglow.find_my_pet.model.Pet;
 import com.sunglow.find_my_pet.model.Poster;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 @Repository
 public interface PosterManagerRepository extends CrudRepository<Poster, Long> {
+    List<Poster> findByPet(Pet pet);
+
 }
