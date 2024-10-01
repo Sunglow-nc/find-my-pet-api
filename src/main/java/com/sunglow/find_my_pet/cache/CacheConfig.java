dc1c5a27375a5afcc4827bb70090bce75373f34b
dimadeloseros1 <dimadeloseros1@gmail.com>
2024-10-01 11:10:41 +0200

    Feat: Implement cache configuration

--- src/main/java/com/sunglow/find_my_pet/cache/CacheConfig.java
+++ src/main/java/com/sunglow/find_my_pet/cache/CacheConfig.java
@@ -1,4 +1,30 @@
 package com.sunglow.find_my_pet.cache;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@EnableCaching
+@Configuration
 public class CacheConfig {
+
+    @Bean
+    public Caffeine<Object, Object> caffeineConfig() {
+        return Caffeine.newBuilder()
+                .initialCapacity(10)
+                .maximumSize(500)
+                .expireAfterWrite(300, TimeUnit.SECONDS);
+    }
+
+    @Bean
+    public CacheManager cacheManager() {
+        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager("postersCache");
+        caffeineCacheManager.setCaffeine(caffeineConfig());
+        return caffeineCacheManager;
+    }
 }
